226d5e84e78a2e3d5e8e5b53e91ef054005850d8
Roger V <60841571+rogerrogerv@users.noreply.github.com>
2020-04-24 03:12:22 +0900

    Add list and creat channels methods

--- models/channels/create.js
+++ models/channels/create.js
@@ -1,8 +1,31 @@
-const validateChannelName = (uName) =>
-  typeof uName === "string" && uName.replace(" ", "").length > 3;
+const validateChannelName = (chanName) =>
+  typeof chanName === "string" && chanName.replace(" ", "").length > 3;
 
 module.exports = (knex, Channel) => {
   return (params) => {
-    return Promise.resolve({}); // fix me!
+    const channelName = params.name;
+
+    if (!validateChannelName(channelName)) {
+      return Promise.reject(
+        new Error("Channel name must be provided, and be at least 3 characters")
+      );
+    }
+
+    return knex("channels")
+      .insert({ name: channelName.toLowerCase() })
+      .then(() => {
+        return knex("channels")
+          .where({ name: channelName.toLowerCase() })
+          .select();
+      })
+      .then((channels) => new Channel(channels.pop()))
+      .catch((error) => {
+        if (
+          error.message.match("duplicate key value") ||
+          error.message.match("Unique constraint failed")
+        )
+          return Promise.reject(new Error("That channel name already exists"));
+        return Promise.reject(error);
+      });
   };
 };
--- models/channels/list.js
+++ models/channels/list.js
@@ -1,5 +1,9 @@
 module.exports = (knex, Channel) => {
   return () => {
-    return Promise.resolve([]); // fix me!
+    return Promise.resolve(knex.select("*").from("channels")) // fix me!
+      .then((dirtyChannels) =>
+        dirtyChannels.map((channel) => new Channel(channel))
+      )
+      .catch((error) => console.log(error));
   };
 };
